31be379462e7e5fa9abe5c0ae204ee635f46231b
Sedat MENGÜ <sedatmengu@gmail.com>
2023-05-13 00:23:28 +0300

    Add files via upload

--- fonksiyonlar-zip.py
+++ fonksiyonlar-zip.py
@@ -8,8 +8,8 @@ liste1=[1,2,3,4]
 liste2=["a","b","c","d"]
 
 print(list(zip(liste1,liste2)))                 # / [(1, 'a'), (2, 'b'), (3, 'c'), (4, 'd')] list yazdığımız için liste olarak dönderdi
-print(set(zip(liste1,liste2)))                  # / {(3, 'c'), (2, 'b'), (4, 'd'), (1, 'a')}  set yazdığımız için dict veri tipinde dönderdi. her çalıştığında farklı dizilimle gelir.
-print(tuple(zip(liste1,liste2)))                # / ((1, 'a'), (2, 'b'), (3, 'c'), (4, 'd'))
+print(set(zip(liste1,liste2)))                  # / {(3, 'c'), (2, 'b'), (4, 'd'), (1, 'a')} set yazdığımız için dict veri tipinde dönderdi. her çalıştığında farklı dizilimle gelir.
+print(tuple(zip(liste1,liste2)))                # / ((1, 'a'), (2, 'b'), (3, 'c'), (4, 'd')) tuple yazdığımız için tuple veri tipinde dönerdir.
 
 
 liste3=list("python")
